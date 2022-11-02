20bc70353d40cde643be65cd2fb5a0dd171a6f41
Hella-Thia <saromsaravuthia@gmail.com>
2022-11-02 14:16:56 +0700

    Adding Container

--- app.vue
+++ app.vue
@@ -1,8 +1,19 @@
 <template>
   <div :class="{'dark': darkMode}">
-    
+    <!-- Starting Tag -->
     <div class="min-h-full">
-      <div></div>
+
+      <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+
+        <!-- Left  Sidebar -->
+
+        <div class="bg-red-500">asd</div>
+
+        <!-- Main Content -->
+
+        <!-- Right Sidebar -->
+      </div>
+      
     </div>
 
   </div>
