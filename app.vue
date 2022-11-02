846161506d886848657ee25f5307478550464345
Hella-Thia <saromsaravuthia@gmail.com>
2022-11-02 14:08:41 +0700

    Installig Tailwind CSS

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div :class="{'dark': darkMode}">
+    
+    <div class="min-h-full">
+      <div></div>
+    </div>
+
   </div>
 </template>
+<script setup>
+  const darkMode = ref(true)
+</script>
